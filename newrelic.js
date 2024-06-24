e1325e40d9e4d5f446710f5eb9d4f9791d1716e4
ApeWithGlasses <wcgiraldo0@gmail.com>
2024-06-23 22:59:03 -0500

    newrelic changes

--- newrelic.js
+++ newrelic.js
@@ -2,7 +2,7 @@
 exports.config = {
   app_name: ["FoodMe"],
   license_key:
-    process.env.LICENSE_KEY || "84cda05dee57b87563f71c39d466aab6879cNRAL",
+    process.env.LICENSE_KEY,
   logging: { level: "info" },
   allow_all_headers: true,
   attributes: {
