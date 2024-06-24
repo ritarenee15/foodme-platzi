23c06de6a9c8197e281ac4494b5a294fa323bbd1
ApeWithGlasses <wcgiraldo0@gmail.com>
2024-06-23 23:34:58 -0500

    newrelic added

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+"use strict";
+const dotenv = require("dotenv");
+dotenv.config();
+
+exports.config = {
+  app_name: ["FoodMe-App"],
+  license_key: process.env.LICENSE_KEY,
+  logging: { level: "info" },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      "request.headers.cookie",
+      "request.headers.authorization",
+      "request.headers.proxyAuthorization",
+      "request.headers.setCookie*",
+      "request.headers.x*",
+      "response.headers.cookie",
+      "response.headers.authorization",
+      "response.headers.proxyAuthorization",
+      "response.headers.setCookie*",
+      "response.headers.x*",
+    ],
+  },
+};
