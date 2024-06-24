78d9a8fcccd06c8a46e5a9ca7901c419d077ae14
ApeWithGlasses <wcgiraldo0@gmail.com>
2024-06-23 22:55:30 -0500

    newrelic not working lol

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,8 @@
 "use strict";
 exports.config = {
-  app_name: ["Foodme"],
-  license_key: process.env.LICENSE_KEY,
+  app_name: ["FoodMe"],
+  license_key:
+    process.env.LICENSE_KEY || "84cda05dee57b87563f71c39d466aab6879cNRAL",
   logging: { level: "info" },
   allow_all_headers: true,
   attributes: {
